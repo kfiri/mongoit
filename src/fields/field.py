618274b9109d9b3d3ca534cfe879a02efc60d41b
Kfir Nissim <kfirr99.gmail.com>
2019-05-04 20:20:46 +0300

    implement defaults and name agruments in Field

--- src/fields/field.py
+++ src/fields/field.py
@@ -5,8 +5,11 @@ class Field(object):
     """A generic class that represents any field from the Mongo database.
     """
 
+    name = None
     nullable = True
     required = True
+    get_default_bson = None
+    get_default_value = None
 
     def __init__(self, **kwargs):
         """Represent a field in a mongodb document/sub-document.
@@ -18,6 +21,8 @@ class Field(object):
           resolve_value -> validate_value -> to_bson -> resolve_bson -> validate_bson -> <insert to mongo>
 
         :Parameters:
+          - `name` (optional): The name of the field in the Mongo database
+            (this is used by :class:`ObjectField`)
           - `nullable` (optional): If ``True`` (the default), allow the value
             of this field to be ``None``. Otherwise, raise :class:`TypeError`
             (unless `default_bson` is specified).
@@ -38,12 +43,36 @@ class Field(object):
         """
         super(Field, self).__init__()
 
+        if 'name' in kwargs:
+            self.required = kwargs.pop('name')
         if 'nullable' in kwargs:
             self.nullable = kwargs.pop('nullable')
         if 'required' in kwargs:
             self.required = kwargs.pop('required')
 
-        # TODO until V0.0.1: implement default bson and default value.
+        get_default_bson = None
+        if 'get_default_bson' in kwargs:
+            get_default_bson = kwargs.pop('get_default_bson')
+        if 'default_bson' in kwargs:
+            if get_default_bson:
+                raise TypeError("cannot use keyword argument 'default_bson' "
+                                "with 'get_default_bson'")
+            default_bson = kwargs.pop('default_bson')
+            get_default_bson = lambda: default_bson
+        if get_default_bson:
+            self.get_default_bson = get_default_bson
+
+        get_default_value = None
+        if 'get_default_value' in kwargs:
+            get_default_value = kwargs.pop('get_default_value')
+        if 'default_value' in kwargs:
+            if get_default_value:
+                raise TypeError("cannot use keyword argument 'default_value' "
+                                "with 'get_default_value'")
+            default_value = kwargs.pop('default_value')
+            get_default_value = lambda: default_value
+        if get_default_value:
+            self.get_default_value = get_default_value
 
         if kwargs:
             raise TypeError("Field() got an unexpected keyword argument(s) "
@@ -69,9 +98,8 @@ class Field(object):
         :Returns:
           The resolved BSON value.
         """
-        # TODO until V0.0.1: implement default bson and default value.
-        # if value is None and not self.nullable and self.get_default_bson:
-        #     return self.get_default_bson()
+        if value is None and not self.nullable and self.get_default_bson:
+            return self.get_default_bson()
         return value
 
     def validate_bson(self, value):
@@ -88,11 +116,14 @@ class Field(object):
                             "is not nullable" % self)
 
     def resolve_value(self, value):
-        """Resolve `value`.
+        """Resolve `value` by setting the default value if `value` is ``None``
+        and not nullable.
 
         :Returns:
           The resolved value.
         """
+        if value is None and not self.nullable and self.get_default_value:
+            return self.get_default_value()
         return value
 
     def validate_value(self, value):
