2cfb07d10afcdd9b313268d3abb8fcbc8baf44d4
Kfir Nissim <kfirr99.gmail.com>
2019-05-04 21:28:36 +0300

    rename string field

--- src/fields/string_field.py
+++ src/fields/string_field.py
@@ -1,18 +1,20 @@
-from bson.py3compat import text_type
+"""A string field within a Mongo document."""
+
+from bson.py3compat import string_type
 
 from .field import Field
 
 
-class TextField(Field):
+class StringField(Field):
     """Represents a string field in the mongo database.
     """
 
     def validate_bson(self, value):
         """Raises :class:`TypeError` if `value` is not an instance of 
-        :class:`str` (:class:`unicode` in python 2).
+        :class:`str` (:class:`basestring` in python 2).
         """
-        if super(TextField, self).validate_bson(value):
+        if super(StringField, self).validate_bson(value):
             return True
-        if not isinstance(value, text_type):
+        if not isinstance(value, string_type):
             raise TypeError("value %s must be an instance of %s" %
-                            (value, text_type.__name__))
+                            (value, string_type.__name__))
