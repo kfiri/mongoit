784f615dea1bfc82687480b71a2f8c8edcd81901
Kfir Nissim <kfirr99.gmail.com>
2019-05-04 21:27:54 +0300

    add numeric fields

--- src/fields/__init__.py
+++ src/fields/__init__.py
@@ -1,3 +1,4 @@
 from .field import Field
 from .boolean_field import BooleanField
 from .string_field import TextField
+from .number_fields import NumberField, IntegerField, RealNumberField
---
+++ src/fields/number_fields.py
@@ -0,0 +1,48 @@
+"""The numeric fields within a Mongo document."""
+
+from bson import Int64
+from bson.py3compat import integer_types
+
+from .field import Field
+
+
+class NumberField(Field):
+    """Represents a number field in the mongo database.
+    """
+
+    def validate_bson(self, value):
+        """Raises :class:`TypeError` if `value` is not an instance of a number.
+        """
+        if super(NumberField, self).validate_bson(value):
+            return True
+        if not (isinstance(value, integer_types) or isinstance(value, float)):
+            raise TypeError("value %r must be an instance of a number" % value)
+
+
+class IntegerField(NumberField):
+    """Represents an integer field in the mongo database.
+    """
+
+    def validate_bson(self, value):
+        """Raises :class:`TypeError` if `value` is not an instance of an
+        integer.
+        """
+        if super(IntegerField, self).validate_bson(value):
+            return True
+        if not isinstance(value, integer_types):
+            raise TypeError("value %r must be an instance of an integer" %
+                            value)
+
+
+class RealNumberField(NumberField):
+    """Represents an real number (float) field in the mongo database.
+    """
+
+    def validate_bson(self, value):
+        """Raises :class:`TypeError` if `value` is not an instance of
+        :class:`float`.
+        """
+        if super(RealNumberField, self).validate_bson(value):
+            return True
+        if not isinstance(value, float):
+            raise TypeError("value %r must be an instance of float" % value)
